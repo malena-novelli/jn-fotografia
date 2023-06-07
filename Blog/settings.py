2f17bce0582f5b69c7c5be0d56f724a598ed1f00
malena-novelli <male.g.novelli@gmail.com>
2023-06-07 00:17:06 -0300

    updated settings file

--- Blog/settings.py
+++ Blog/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8uzkfqv7(=lz&r61sf&8#%vzf_==r!48fc=2r@$mb#)4rum2@p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['147.182.218.0', 'www.juannovelli.com', 'juannovelli.com']
 
